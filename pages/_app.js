e24b358bd913e28340c28e71e0c08ba84d219350
electricblues <65368545+electricblues@users.noreply.github.com>
2022-05-30 21:41:57 +0100

    added navbar & changed sccs

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import "../styles/globals.css";
-
 import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
+
+import "../styles/globals.css";
+import "../styles/styles.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,25 @@
-import Image from "next/image";
-import dynamic from "next/dynamic";
-import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
-import { Card } from "primereact/card";
-import { Button } from "primereact/button";
-
-const PictureWrapper = ({ image, children }) => {
-  return (
-    <div className="picture-wrapper">
-      <div className="picture-wrapper-item">
-        <div
-          style={{
-            height: "100%",
-            minHeight: "40rem",
-            width: "100%",
-            position: "relative",
-            backgroundSize: "cover",
-            backgroundImage: `url(${image})`,
-          }}
-        >
-          {/**
-         * 
-        <Image layout="fill" objectFit="contain" src={image} alt={"boat"} />  */}
-        </div>
-      </div>
-      <div className="picture-wrapper-item picture-wrapper-child">
-        {children}
-      </div>
-    </div>
-  );
-};
-
-const Intro = () => (
-  <div style={{ padding: "5rem", textAlign: "center" }}>
-    <p>Start To Improve Your Life</p>
-    <div style={{ paddingBottom: "2rem" }}>
-      <span style={{ fontSize: "4rem", fontWeight: "100" }}>
-        Find Purpose & Create The Life That You Want
-      </span>
-    </div>
-    <Button label="Proceed" className="p-button-raised p-button-rounded" />
-  </div>
-);
+import MenuBar from "./components/Display/MenuBar";
+import Intro from "./components/Display/Intro";
+import PictureWrapper from "./components/Display/PictureWrapper";
 
 export default function Home() {
   return (
     <div
       style={{
         padding: "3rem",
-        backgroundColor: "#5BC4F7",
+        paddingTop: "0px",
+        backgroundColor: "#ADCBCB",
         minHeight: "100vh",
         height: "100%",
       }}
     >
       <div style={{ maxWidth: "100rem", margin: "auto" }}>
-        <PictureWrapper image={"/images/boat.jpg"}>
+        <MenuBar />
+        <br />
+        <PictureWrapper image={"/images/boat.jpg"} left={false}>
           <Intro />
         </PictureWrapper>
+        <br />
       </div>
     </div>
   );
