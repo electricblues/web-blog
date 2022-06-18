2876c49cdce41b91a52fe07c87a4777668f7f694
Tudor Neacsu <neacsutudorvlad@outlook.com>
2022-06-18 08:11:07 +0100

    build issues

--- pages/components/Themes/One.js
+++
@@ -1,5 +0,0 @@
-import "primereact/resources/themes/saga-green/theme.css";
-
-export default function One() {
-  return <></>;
-}
--- pages/components/Themes/Two.js
+++
@@ -1,5 +0,0 @@
-import "primereact/resources/themes/md-dark-indigo/theme.css";
-
-export default function One() {
-  return <></>;
-}
