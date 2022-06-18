a6196deef883f8ad5782bd1b5681fd196aab02a4
Tudor Neacsu <neacsutudorvlad@outlook.com>
2022-06-18 07:56:58 +0100

    build issue

--- pages/components/Themes/WayToAddTheme.js
+++ pages/components/Themes/WayToAddTheme.js
@@ -1,30 +1,29 @@
 import dynamic from "next/dynamic";
-import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
-import { Button } from "primereact/button";
+// import { useEffect, useState } from "react";
+// import { Button } from "primereact/button";
 
-const ThemeOne = dynamic(() => import("./components/Themes/One"));
-const ThemeTwo = dynamic(() => import("./components/Themes/Two"));
+// const ThemeOne = dynamic(() => import("./components/Themes/One"));
+// const ThemeTwo = dynamic(() => import("./components/Themes/Two"));
 
-// DO NOT IMPORT THIS AT ALL
-export default function Home() {
-  const [tg, setTg] = useState();
-  const onClick = () => {
-    localStorage.setItem("theme", !tg);
-    console.log("on click  ", tg);
-    window.location.reload();
-  };
+// // DO NOT IMPORT THIS AT ALL
+// export default function Home() {
+//   const [tg, setTg] = useState();
+//   const onClick = () => {
+//     localStorage.setItem("theme", !tg);
+//     console.log("on click  ", tg);
+//     window.location.reload();
+//   };
 
-  useEffect(() => {
-    const theme = localStorage.getItem("theme");
-    setTg(theme === "true");
-    console.log("on load  ", theme, theme === "true");
-  }, []);
+//   useEffect(() => {
+//     const theme = localStorage.getItem("theme");
+//     setTg(theme === "true");
+//     console.log("on load  ", theme, theme === "true");
+//   }, []);
 
-  return (
-    <div className={styles.container}>
-      {!!tg ? <ThemeOne /> : <ThemeTwo />}
-      <Button onClick={onClick}>omfg</Button>
-    </div>
-  );
-}
+//   return (
+//     <div className={styles.container}>
+//       {!!tg ? <ThemeOne /> : <ThemeTwo />}
+//       <Button onClick={onClick}>omfg</Button>
+//     </div>
+//   );
+// }
